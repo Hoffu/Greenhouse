3a8d8bd561311a4915ece41862f5157f137c8c06
Kirill Petrov <hoffusha@gmail.com>
2021-05-22 01:56:19 +0300

    Rewrote broker

--- src/model/Broker.java
+++ src/model/Broker.java
@@ -1,23 +1,42 @@
 package model;
 
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import model.controller.Event;
 import sample.Controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Broker {
     private Controller controller;
+    private HashMap<Class<?>, Color> eventColorHashMap = new HashMap<>();
+    private HashMap<String, Circle> circlesHashMap = new HashMap<>();
 
     public Broker(Controller controller) {
         this.controller = controller;
+        eventColorHashMap.put(GreenhouseControls.LightOn.class, Color.DARKGREEN);
+        eventColorHashMap.put(GreenhouseControls.LightOff.class, Color.DARKRED);
+        eventColorHashMap.put(GreenhouseControls.WaterOn.class, Color.DARKGREEN);
+        eventColorHashMap.put(GreenhouseControls.WaterOff.class, Color.DARKRED);
+        eventColorHashMap.put(GreenhouseControls.ThermostatDay.class, Color.YELLOW);
+        eventColorHashMap.put(GreenhouseControls.ThermostatNight.class, Color.DARKGRAY);
+
+        circlesHashMap.put("Light", controller.lightCircle);
+        circlesHashMap.put("Water", controller.waterCircle);
+        circlesHashMap.put("Thermostat", controller.thermCircle);
     }
 
     public void checkEvent(Event e) {
         controller.label.setText(e.toString());
-        if (e instanceof GreenhouseControls.LightOn) controller.lightCircle.setFill(Color.DARKGREEN);
-        if (e instanceof GreenhouseControls.LightOff) controller.lightCircle.setFill(Color.DARKRED);
-        if (e instanceof GreenhouseControls.WaterOn) controller.waterCircle.setFill(Color.DARKGREEN);
-        if (e instanceof GreenhouseControls.WaterOff) controller.waterCircle.setFill(Color.DARKRED);
-        if (e instanceof GreenhouseControls.ThermostatDay) controller.thermCircle.setFill(Color.YELLOW);
-        if (e instanceof GreenhouseControls.ThermostatNight) controller.thermCircle.setFill(Color.DARKGRAY);
+        for(Map.Entry<Class<?>, Color> entryEvent : eventColorHashMap.entrySet()) {
+            if (entryEvent.getKey() == e.getClass()) {
+                for(Map.Entry<String, Circle> entryCircles : circlesHashMap.entrySet()) {
+                    if (e.toString().startsWith(entryCircles.getKey())) {
+                        entryCircles.getValue().setFill(entryEvent.getValue());
+                    }
+                }
+            }
+        }
     }
 }
